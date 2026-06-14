f79a4684d754c057e3cec1effc2c1bdedd859803
Pawanjadon32 <142287917+Pawanjadon32@users.noreply.github.com>
2026-06-14 22:40:58 +0530

    Add task to create application info file

--- roles/nginx/tasks/main.tf
+++ roles/nginx/tasks/main.tf
@@ -1,1 +1,7 @@
-
+---
+- name: Create application file
+  copy:
+    content: |
+      Deployment Time: {{ ansible_date_time.iso8601 }}
+      Hostname: {{ inventory_hostname }}
+    dest: /tmp/app-info.txt
