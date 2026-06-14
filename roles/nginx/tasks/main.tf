d53a92612bbd1c704ff394c87385959c07ec8991
Pawanjadon32 <142287917+Pawanjadon32@users.noreply.github.com>
2026-06-14 23:08:50 +0530

    Delete roles/nginx/tasks/main.tf

--- roles/nginx/tasks/main.tf
+++
@@ -1,7 +0,0 @@
----
-- name: Create application file
-  copy:
-    content: |
-      Deployment Time: {{ ansible_date_time.iso8601 }}
-      Hostname: {{ inventory_hostname }}
-    dest: /tmp/app-info.txt
